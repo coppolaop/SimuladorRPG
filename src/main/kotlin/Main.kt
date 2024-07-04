0a7d9f69a6414e7846ed35aedd2d53c23852569e
Marcos Gonçalves <mag.junior@hotmail.com>
2024-07-04 10:03:01 -0300

    Fix: Tratativas de valores de entrada e remoção de pasta interna

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,14 +6,22 @@ import java.io.File
 
 fun main() {
     println("Bem vindo ao Simulador de combates de RPG")
-    File("resources/pdm").mkdirs()
+    File(PersonagemService.pdmDirectory).mkdirs()
     val monstro = escolherMonstro()
     println("Insira a quantidade que você deseja de monstros desse tipo: ")
     var quantidade = readLine()
     quantidade = if (quantidade.isNullOrBlank()) null else quantidade
+    if (quantidade == "0") {
+        println("A taxa de vitória contra nenhum monstro é sempre 100%, se é que você pode chamar isso de vitória")
+        return
+    }
     println("Informe o nível desejado do grupo de aventureiros: ")
     var nivel = readLine()
     nivel = if (nivel.isNullOrBlank()) null else nivel
+    if (nivel == "0") {
+        println("Personagens de nivel 0 ainda não estão prontos para se aventurar. A taxa de vitória é de 0%.")
+        return
+    }
 
     SimuladorService(monstro ?: "gnoll", quantidade?.toInt() ?: 1)
         .ativarAcaoTripla()
@@ -23,31 +31,19 @@ fun main() {
 }
 
 fun escolherMonstro(): String? {
-    var monstros = PersonagemService.listarMonstros(false)
-    imprimirListaMonstros(monstros, false)
-    println("Escolha um monstro (pasta resources/pdm) da lista pelo número: ")
+    val monstros = PersonagemService.listarMonstros()
+    imprimirListaMonstros(monstros)
+    println("Escolha um monstro (pasta ${PersonagemService.pdmDirectory}) da lista pelo número: ")
     var monstro = readLine()
-
-    if (monstro.equals("x", true)) {
-        monstros = PersonagemService.listarMonstros(true)
-        imprimirListaMonstros(monstros, true)
-        println("Escolha um monstro da lista pelo número: ")
-        monstro = readLine()
-    }
-
     monstro = if (monstro.isNullOrBlank()) null else monstros[monstro.toInt()]
-
     return monstro
 }
 
-fun imprimirListaMonstros(monstros: List<String>, isInternal: Boolean) {
+fun imprimirListaMonstros(monstros: List<String>) {
     for (linha in 0..monstros.size / 5) {
         for (i in 0..4) {
             val position = linha * 5 + i
             if (monstros.size <= position) {
-                if (!isInternal) {
-                    print("| x - Listar monstros internos do sistema |")
-                }
                 break
             }
             print("| $position - ${monstros[position]} |")
